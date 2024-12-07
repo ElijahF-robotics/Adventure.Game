6b6e245ea84cfe0c397941942416963ef50ddd7c
ElijahF-robotics <elijahflader@gmail.com>
2024-12-07 10:47:24 -0500

    Add main file:

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+*    @file: appointment_main.cc
+ *   @author: Elijah Flader
+ *   @date: Dec 7, 2024
+ *   @brief:
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cctype>
+
+#include "appointment.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    // Load data into the appointments vector
+
+}
